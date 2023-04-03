c155a9abe454843e6eea2df48d35bac1290fd932
msg824 <msg824@naver.com>
2023-04-04 00:19:06 +0900

    feat: 헤더에 프로필 이미지, 타이틀, 아이콘 버튼 추가

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,27 +1,77 @@
-import * as React from "react";
+import * as React from 'react';
 import styled from 'styled-components';
+import { Github } from 'styled-icons/boxicons-logos';
+import { LightMode, DarkMode } from 'styled-icons/material-outlined';
 
 const StyledHeader = styled.div`
-  width: 100%;
-  height: 80px;
   display: flex;
+  align-items: center;
+  height: 80px;
   background-color: aliceblue;
   padding: 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
 `;
 
-const Profile = styled.div`
+const Profile = styled.img`
   width: 60px;
   height: 60px;
+  border: 1px solid gray;
   border-radius: 50%;
 `;
 
-const Header = () => {
+const IconButton = styled.button`
+  padding: 8px;
+  background-color: transparent;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  user-select: none;
+  transition: background-color 0.3s cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.04);
+    transition: 0.3s;
+  }
+`;
 
+const Header = () => {
   return (
     <StyledHeader>
-      <Profile>
-        hello
-      </Profile>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Profile src={'/images/profile.png'} alt={'profile'} />
+
+          <div
+            style={{
+              marginLeft: '20px',
+            }}
+          >
+            <h2>Minsung's Blog</h2>
+          </div>
+        </div>
+
+        <div>
+          <IconButton onClick={() => alert('구현 준비중입니다.')}>
+            <DarkMode width={30} height={30} />
+          </IconButton>
+          <IconButton
+            onClick={() => window.open('https://github.com/minssung', '_blank')}
+          >
+            <Github width={30} height={30} />
+          </IconButton>
+        </div>
+      </div>
     </StyledHeader>
   );
 };
